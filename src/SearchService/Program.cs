63548a02460de091cd8c1e406a02f56d5c2668d1
Rebecca Brietzke <rnbrietzke@outlook.com>
2024-04-24 11:01:13 -0500

    set up search microservice

---
+++ src/SearchService/Program.cs
@@ -0,0 +1,52 @@
+using System.Net;
+using Polly;
+using Polly.Extensions.Http;
+using Polly.Retry;
+using SearchService.Data;
+using SearchService.Models;
+using SearchService.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+builder.Services.AddHttpClient<AuctionSvcHttpClient>().AddPolicyHandler(GetPolicy());
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+
+
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Lifetime.ApplicationStarted.Register(async () =>
+{
+    try
+    {
+        await DbInitializer.InitDb(app);
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine(e);
+    }
+
+});
+
+
+app.Run();
+
+
+static AsyncRetryPolicy<HttpResponseMessage> GetPolicy() => 
+    HttpPolicyExtensions.HandleTransientHttpError()
+        .OrResult(msg => msg.StatusCode == HttpStatusCode.NotFound)
+        .WaitAndRetryForeverAsync(_ => TimeSpan.FromSeconds(3));
