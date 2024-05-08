2070b5432f02e5be082543f2420da361ff5343b5
Rebecca Brietzke <rnbrietzke@outlook.com>
2024-05-08 11:54:41 -0500

    add remaining code to allow for dockerizing the services

---
+++ src/AuctionService/Consumers/BidPlacedConsmer.cs
@@ -0,0 +1,29 @@
+using AuctionService.Data;
+using Contracts;
+using MassTransit;
+
+namespace AuctionService.Consumers;
+
+public class BidPlacedConsmer : IConsumer<BidPlaced>
+{
+    private readonly AuctionDbContext _dbContext;
+
+    public BidPlacedConsmer(AuctionDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+    
+    public async Task Consume(ConsumeContext<BidPlaced> context)
+    {
+        var auction = await _dbContext.Auctions.FindAsync(context.Message.AuctionId);
+
+        if (auction.CurrentHighBid == null || context.Message.BidStatus.Contains("Accepted") &&
+            context.Message.Amount > auction.CurrentHighBid)
+        {
+            auction.CurrentHighBid = context.Message.Amount;
+            await _dbContext.SaveChangesAsync();
+        }
+        
+        
+    }
+}
